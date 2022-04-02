faf528b0d9d82a143da2b2f375e32a0e54b04319
Victor Koenders <gpg@trangar.com>
2022-04-02 11:07:49 +0200

    Wiggle wiggle wiggle wiggle

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
+use std::time::Instant;
+
 use macroquad::prelude::*;
 
 #[macroquad::main("Potat")]
 async fn main() {
-    while !is_key_pressed(KeyCode::Enter) {
-        clear_background(BLACK);
-        next_frame().await;
-    }
+    // good for showing off the intro
+    // while !is_key_pressed(KeyCode::Enter) {
+    //     clear_background(BLACK);
+    //     next_frame().await;
+    // }
 
     Dialogue::show(|d| {
         d.big_text("Day 0");
         d.text("Uh. Dear diary? I guess?");
         d.text("Today was shit.");
         d.text("I was in my potato field like normal, when the sirens started ringing.");
+        d.lines.push(Line::Jiggle {
+            text: String::from("It was terr.. terrif.. scary!"),
+            color: WHITE,
+        });
         d.text("Luckily we had that shelter training last week.");
         d.text("I didn't get hurt, luckily, but the ground shook.");
         d.text("Anyway I'm now stuck in here.");
@@ -62,11 +69,29 @@ impl Dialogue {
     pub async fn show(constructor: impl FnOnce(&mut Dialogue)) {
         Self::new(constructor).render().await;
     }
+    pub fn big_color_text(&mut self, text: impl Into<String>, color: Color) {
+        self.lines.push(Line::BigText {
+            text: text.into(),
+            color,
+        });
+    }
+    pub fn color_text(&mut self, text: impl Into<String>, color: Color) {
+        self.lines.push(Line::Text {
+            text: text.into(),
+            color,
+        });
+    }
     pub fn big_text(&mut self, text: impl Into<String>) {
-        self.lines.push(Line::BigText(text.into()));
+        self.lines.push(Line::BigText {
+            text: text.into(),
+            color: WHITE,
+        });
     }
     pub fn text(&mut self, text: impl Into<String>) {
-        self.lines.push(Line::Text(text.into()));
+        self.lines.push(Line::Text {
+            text: text.into(),
+            color: WHITE,
+        });
     }
 
     pub async fn render_with_events(self, mut e: impl FnMut(FrameCtx) -> Event) {
@@ -74,6 +99,7 @@ impl Dialogue {
         let mut char_idx = 0;
 
         let mut pause_time = 0;
+        let start = Instant::now();
 
         loop {
             clear_background(BLACK);
@@ -81,13 +107,14 @@ impl Dialogue {
             let x = 50.;
             let mut y = 50.;
 
+            let timestamp = start.elapsed().as_secs_f32();
             for line in &self.lines[..line_idx] {
-                let height = line.draw(x, y, None);
+                let height = line.draw(timestamp, x, y, None);
                 y += height;
             }
 
             if let Some(line) = self.lines.get(line_idx) {
-                line.draw(x, y, Some(char_idx));
+                line.draw(timestamp, x, y, Some(char_idx));
             } else {
                 draw_text(
                     "<ENTER>",
@@ -158,19 +185,21 @@ pub enum Event {
 }
 
 enum Line {
-    BigText(String),
-    Text(String),
+    BigText { text: String, color: Color },
+    Text { text: String, color: Color },
+    Jiggle { text: String, color: Color },
 }
 
 impl Line {
     pub fn str(&self) -> &str {
         match self {
-            Self::BigText(t) => t,
-            Self::Text(t) => t,
+            Self::BigText { text, .. } => text,
+            Self::Text { text, .. } => text,
+            Self::Jiggle { text, .. } => text,
         }
     }
 
-    pub fn draw(&self, x: f32, y: f32, len: Option<usize>) -> f32 {
+    pub fn draw(&self, timestamp: f32, x: f32, y: f32, len: Option<usize>) -> f32 {
         let mut str = self.str();
         if let Some(len) = len {
             let mut indices = str.char_indices();
@@ -180,13 +209,40 @@ impl Line {
         }
 
         match self {
-            Line::BigText(_) => {
-                draw_text(str, x, y, 40., WHITE);
+            Line::BigText { color, .. } => {
+                draw_text(str, x, y, 40., *color);
                 80.
             }
-            Line::Text(_) => {
-                draw_text(str, x, y, 24., WHITE);
+            Line::Text { color, .. } => {
+                draw_text(str, x, y, 24., *color);
                 30.
+            }
+            Line::Jiggle { color, .. } => {
+                const SPACING: f32 = 0.;
+                const STEP: f32 = 5.;
+                const DISTANCE: f32 = 2.;
+                const SPEED: f32 = 10.;
+
+                let mut x = x + SPACING;
+                let y = y + SPACING;
+                for (index, char) in str.chars().enumerate() {
+                    let mut buffer = [0u8; 4];
+                    let str = char.encode_utf8(&mut buffer);
+                    let size = measure_text(str, None, 24, 1.0);
+
+                    let angle = (timestamp + (index as f32 / 10.)) * SPEED;
+                    draw_text(
+                        str,
+                        x + angle.cos() * DISTANCE,
+                        y + angle.sin() * DISTANCE,
+                        24.,
+                        *color,
+                    );
+
+                    x += size.width + STEP;
+                }
+
+                30. + SPACING * 2.
             }
         }
     }
